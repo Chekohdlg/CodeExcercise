f7265636b620501a3f6ee27b60861006c777202c
Sergio Hidalgo <sergio.hidalgo@unosquare.com>
2022-02-14 18:07:13 -0600

    Add project files.

---
+++ CodeExcercise/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using CodeExercise.DataAcccess.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeExcercise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public ActionResult GetAllCategories()
+        {
+            var categories = _db.Categories.AsNoTracking();
+            return Ok(categories);
+        } 
+    }
+}

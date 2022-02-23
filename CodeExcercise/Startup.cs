ecc7cdb4f74053c8c13acfe8ecff313c3c710301
Sergio Hidalgo <sergio.hidalgo@unosquare.com>
2022-02-23 09:55:26 -0600

    Add Cors Add Crud interface

--- CodeExcercise/Startup.cs
+++ CodeExcercise/Startup.cs
@@ -55,6 +55,12 @@ namespace CodeExcercise
 
             app.UseRouting();
 
+            app.UseCors(x => x
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .SetIsOriginAllowed(origin => true) // allow any origin
+              .AllowCredentials()); // allow credentials
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ CodeExercise_Model/Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExercise_Model.Interfaces
+{
+    public interface ICrud<T> where T : class
+    {
+        Task<List<T>> GetList();
+        Task<T> GetById(int id);
+        Task<T> DeleteById(int id);
+
+        Task<T> Update(T entity);
+    }
+}
